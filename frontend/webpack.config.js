f8ad0c1a76b4831ea75b18b16998b7500a0ecfec
joris <jorisvddonk@gmail.com>
2019-10-20 15:50:02 +0200

    parcel -> webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path"),
+  webpack = require("webpack"),
+  HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: ["./main.tsx", "webpack-hot-middleware/client"],
+    vendor: ["react", "react-dom"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].bundle.js"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader"
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html")
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 1234
+  },
+  node: {
+    fs: "empty"
+  }
+};
